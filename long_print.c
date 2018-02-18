38e9f9239b800f933d093cdcaf1fbcfa2fc83a7b
vtolochk <v.tolochko13@gmail.com>
2018-02-18 17:42:55 +0200

    first 2 columns are ready

--- long_print.c
+++ long_print.c
@@ -12,7 +12,7 @@
 
 #include "ft_ls.h"
 
-unsigned int ft_get_blocks(char **files, t_ls *data, unsigned int *link_width)
+static void write_blocks(char **files, t_ls *data, unsigned int *link_width)
 {
 	char *temp;
 	unsigned int i;
@@ -22,7 +22,6 @@ unsigned int ft_get_blocks(char **files, t_ls *data, unsigned int *link_width)
 
 	i = 0;
 	blocks = 0;
-	temp_width = 0;
 	while (files[i])
 	{
 		temp = ft_strjoin(data->path_to_dir, files[i++]);
@@ -33,28 +32,43 @@ unsigned int ft_get_blocks(char **files, t_ls *data, unsigned int *link_width)
 		blocks += status.st_blocks;
 		free(temp);
 	}
-	return (blocks);
+	ft_printf("total %d\n", blocks);
 }
 
-void print_file_mode(char *file, mode_t st_mode)
+static void write_file_type(char *file)
 {
-	unsigned int i;
-	unsigned int k;
 	char c;
+	struct stat status;
+
+	c = 0;
+	lstat(file, &status);
+	if (S_ISBLK(status.st_mode))
+		c = 'b';
+	else if (S_ISCHR(status.st_mode))
+		c = 'c';
+	else if (S_ISDIR(status.st_mode))
+		c = 'd';
+	else if (S_ISFIFO(status.st_mode))
+		c = 'f';
+	else if (S_ISREG(status.st_mode))
+		c = '-';
+	else if (S_ISLNK(status.st_mode))
+		c = 'l';
+	else if (S_ISSOCK(status.st_mode))
+		c = 's';
+	write(1, &c, 1);
+}
+
+static void print_file_mode(char *file, mode_t st_mode, unsigned int k, unsigned int i)
+{
 	char *temp;
 
-	k = 0;
 	temp = ft_itoa_base(st_mode, 8);
 	i = (unsigned int)ft_strlen(temp) - 4;
-	//c = ft_get_file_type(file); // it doesnt work:( need to create another function :(
-	file++;
-	c = 'd';
-	write(1, &c, 1);
-	//
-	while (k != 3)
+	write_file_type(file);
+	while (k++ != 3)
 	{
-		i++;
-		if (temp[i] == '0')
+		if (temp[++i] == '0')
 			write(1, NO_ACCESS, 3);
 		else if (temp[i] == '1')
 			write(1, X_ACCESS, 3);
@@ -70,37 +84,33 @@ void print_file_mode(char *file, mode_t st_mode)
 			write(1, RW_ACCESS, 3);
 		else if (temp[i] == '7')
 			write(1, RWX_ACCESS, 3);
-		k++;
 	}
+	free(temp);
 }
 
 void ft_long_print(char **files, t_ls *data)
 {
 	unsigned int i;
-	unsigned int blocks;
 	unsigned int link_width;
 	struct stat status;
 	char *temp;
 
 	i = 0;
-	blocks = 0;
 	link_width = 0;
 	if (files[i])
 	{
 		temp = data->path_to_dir;
 		data->path_to_dir = ft_strjoin(data->path_to_dir, "/");
 		free(temp);
-		blocks = ft_get_blocks(files, data, &link_width);
-		ft_printf("total %d\n", blocks);
+		write_blocks(files, data, &link_width);
 	}
 	while (files[i])
 	{
 		temp = ft_strjoin(data->path_to_dir, files[i]);
 		lstat(temp, &status);
-		print_file_mode(temp, status.st_mode);
+		print_file_mode(temp, status.st_mode, 0, 0);
 		ft_printf(" %*u ", link_width + 1, status.st_nlink);
-		ft_printf("%s\n", files[i]);
-		i++;
+		ft_printf("%s\n", files[i++]);// file name
 		free(temp);
 	}
 	free(data->path_to_dir);
